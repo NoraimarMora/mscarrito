9cce479cef72ab2725eb08adec4bf90174fcc81e
Noraimar Mora <noraimar.mora@gmail.com>
2019-10-22 16:51:16 -0400

    Añadido custom schema type

--- models/cliente.js
+++ models/cliente.js
@@ -1,10 +1,11 @@
 'use strict'
 
+var MicroserviceId = require('../customSchemaType/MicroserviceId');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ClienteSchema = Schema({
-    client_id: {type: String, required: true},  // ID en Ms Usuarios
+    // _id: ID en Ms Usuarios
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
     addresses: [{type: Schema.Types.ObjectId, ref: 'Direccion'}]
--- models/elementoCarrito.js
+++ models/elementoCarrito.js
@@ -1,14 +1,15 @@
 'use strict'
 
+var MicroserviceId = require('../customSchemaType/MicroserviceId');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ElementoCarritoSchema = Schema({
     cart_id: {type: Schema.Types.ObjectId, ref: 'Carrito', required: true},
-    product: {type: Schema.Types.ObjectId, ref: 'Producto', required: true}, // ID en Ms Catalogo
+    product: {type: Schema.Types.MicroserviceId, ref: 'Producto', required: true}, // ID en Ms Catalogo
     quantity: {type: Number, required: true},
     unit_price: {type: Number, required: true},
-    features: [{type: Schema.Types.ObjectId, ref: 'Caracteristica'}]
+    features: [{type: Schema.Types.MicroserviceId, ref: 'Caracteristica'}]
 });
 
 module.exports = mongoose.model('ElementoCarrito', ElementoCarritoSchema);
